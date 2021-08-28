ad90c0ad1de88b4a5a04c942f9106148e13cb16a
austinlyon <austin.lyon@gmail.com>
2021-08-27 23:00:37 -0700

    minor correction to titleScene's reference to game.music

--- src/classes/game/title/titleScene.js
+++ src/classes/game/title/titleScene.js
@@ -43,7 +43,8 @@ export default class TitleScene {
   }
 
   exitTitleScreen() {
-    this.titleMusic.src = '';
+    this.game.music.pause();
+    this.game.music.src = '';
     this.unregisterHandlers();
     this.game.loadScene('pong');
   }
